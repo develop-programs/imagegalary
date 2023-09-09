5f4c2576ffbdb30e13feead03f0e0644ef326a32
Shreyansh Awadhiya <131341442+develop-programs@users.noreply.github.com>
2023-09-09 17:13:58 +0530

    updated

--- src/Images/Photos/index.tsx
+++ src/Images/Photos/index.tsx
@@ -5,19 +5,43 @@ import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../services/Redux/Store'
 import { fetchProduct } from '../../services/Redux/Reducers/Photos'
 import FloatingActionButton from '../../components/Ui/FloatingActionButton'
+import { Box, CircularProgress } from '@mui/material'
 
 export default function Photos() {
+    const [Loading, setLoading] = React.useState(true)
     const dispatch = useDispatch<AppDispatch>()
     React.useEffect(() => {
         dispatch(fetchProduct())
+        setTimeout(() => {
+            setLoading(false)
+        }, 6000)
     }, [])
     return (
         <>
-            <Typography variant="body1" color="inherit" paddingY={2} fontSize={20} fontWeight={450}>
-                Photos
-            </Typography>
-            <CardItems />
-            <FloatingActionButton />
+            {
+                Loading ? <>
+                    <Loader />
+                </> : <>
+                    <Typography variant="body1" color="inherit" paddingY={2} fontSize={20} fontWeight={450}>
+                        Photos
+                    </Typography>
+                    <CardItems />
+                    <FloatingActionButton />
+                </>
+            }
+
+        </>
+    )
+}
+
+export function Loader() {
+    return (
+        <>
+            <Box sx={{ width: "100%", height: "15vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Typography variant="body1" color="inherit" display="flex" gap={2} alignItems="center" fontSize={22} fontWeight={550}>
+                    Fetching Your Data Please Wait  <CircularProgress size={30} thickness={5} />
+                </Typography>
+            </Box>
         </>
     )
 }
